db17c8630abe5343075b3503a61e8f6e1f245fd0
agvan <agvandyke25@gmail.com>
2023-09-05 22:56:22 -0500

    added Document() function

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,15 @@
 // controls initial response from the server
+
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
