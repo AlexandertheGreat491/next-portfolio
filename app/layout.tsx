6f9bfbc2392eb34249c755c3272c8b9d66ba2c8b
agvan <agvandyke25@gmail.com>
2023-09-05 22:41:18 -0500

    added one comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 // https://nextjs.org/docs/getting-started/installation
 // defined at the top level of app directory & applies to all routes
+// layout enables modification of initial html returned from the server
 
-export default function RootLayout( {
+export default function RootLayout({
     children,
-} : {
+  }: {
     children: React.ReactNode
-}) {
-    return(
-        <html lang="en">
-       <body>
-        {children}
-        </body>
-        </html>
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
     )
-}
+  }
