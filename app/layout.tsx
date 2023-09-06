d8d9758866e2aafe1df602bfe90ba449e43a7803
agvan <agvandyke25@gmail.com>
2023-09-05 22:40:05 -0500

    created root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
 // https://nextjs.org/docs/getting-started/installation
+// defined at the top level of app directory & applies to all routes
+
+export default function RootLayout( {
+    children,
+} : {
+    children: React.ReactNode
+}) {
+    return(
+        <html lang="en">
+       <body>
+        {children}
+        </body>
+        </html>
+    )
+}
